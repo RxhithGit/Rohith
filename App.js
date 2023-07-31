fcc2dc21f1f098096fefd3e2407218fc07e2db12
Rohith kumar <138948702+RxhithGit@users.noreply.github.com>
2023-07-31 16:56:13 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,49 +1,84 @@
-import "./styles.css";
-import { useState } from "react";
-export default function App() {
-  const [tasks, setTasks] = useState([{}]);
-  const [value, setValues] = useState("");
+import React from "react";
+import { useFormik } from "formik";
 
-  function addItem(e) {
-    e.preventDefault();
-    if (!value) return;
-    const newTask = [...tasks, { text: `${value}` }];
-    setTasks(newTask);
-    setValues("");
-  }
-  function removeItem(e, id) {
-    e.preventDefault();
-    let temp = [...tasks];
-    temp.splice(id, 1);
-    setTasks(temp);
-  }
+export default function App() {
+  const formik = useFormik({
+    initialValues: {
+      Name: "",
+      email: "",
+      password: ""
+    },
+    onSubmit: (values) => {
+      alert(`LOGIN SUCCESSFUL`);
+    },
+    validate: (values) => {
+      const errors = {};
+      if (!values.Name) {
+        errors.Name = "Required";
+      } else if (values.Name.length > 15) {
+        errors.Name = "Must be 15 characters or less";
+      }
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+      if (!values.password) {
+        errors.password = "Required";
+      } else if (values.password.length < 8) {
+        errors.password = "Set Passwords using #,@,Numbers,upper & lowercase letters";
+      }
+      return errors;
+    }
+  });
 
   return (
-    <>
-      <div className="name">
-        <h1>TO - DO LIST</h1>
-      </div>
-      <div>
-        <form className="form" onSubmit={addItem}>
-          <input
-            type="text"
-            className="input"
-            placeholder="Add"
-            value={value}
-            onChange={(e) => setValues(e.target.value)}
-          />
-          <button className="btn_submit">Submit</button>
-        </form>
+    <div style={{ backgroundColor: "red", width: "230px", minHeight: "180px" }}>
+      <div
+        style={{
+          padding: "10px 15px",
 
-        {tasks.map((item, id) => (
-          <div className="task" key={id} id={id}>
-            {item.text}
-            <button className="btn_remove" onClick={(e) => removeItem(e, id)}>
-              X
-            </button>
+          textAlign: "center",
+          color: "black"
+        }}
+      >
+        <form onSubmit={formik.handleSubmit}>
+          <div>
+            <label htmlFor="Name"> User Name:</label>
+            <input
+              id="Name"
+              name="Name"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.Name}
+            />
           </div>
-        ))}
+          {formik.errors.Name ? <div>{formik.errors.Name}</div> : null}
+
+          <label htmlFor="email"> EmailId </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+          <div>
+            <label for="passowrd">Password </label>
+            <input
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+          </div>
+          {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+          <button type="submit">Submit</button>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
